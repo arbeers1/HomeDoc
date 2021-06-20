265158aae0196cbc88bd368e4dbcef9aa2319e9c
Alex Beers <arbeers@wisc.edu>
2021-06-19 19:00:00 -0500

    Updated for new routing

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, session
+from flask import render_template
+import random
+import string
+
+app = Flask(__name__)
+letters = string.ascii_lowercase
+app.secret_key = ''.join(random.choice(letters) for i in range(15))
+ 
+@app.route("/")
+def home():
+    if(session.get("vis", None) == None):
+        val = 0
+        val2 = "auto"
+    else:
+        val = session.get("vis", None)
+        val2 = "none"
+    return render_template('index.html', vis = val, pointer = val2)
+
+@app.route("/lights")
+def lights():
+    session["vis"] = 1
+    return render_template('lights.html')
