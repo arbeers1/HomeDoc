90f9de562d5f5cfab866d07fbed314d1601e9cc9
Alex Beers <arbeers@wisc.edu>
2021-07-06 14:15:06 -0500

    Updated Styling

---
+++ static/scripts/mail_reader.js
@@ -0,0 +1,7 @@
+function read_mail(id){
+    //TODO: Retrieve email by id
+
+    document.getElementById("sender").innerText = "Alex Beers <arbeers@wisc.edu>"
+    document.getElementById("subject").innerText = "Completed Project"
+    document.getElementById("message").innerHTML = "I have attached our completed project files to this email.<br>Thanks,<br><br>Alex"
+}
