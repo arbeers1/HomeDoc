ab2a5b251f88575d377ec5ea1c3e16ea7d172c3b
Alex Beers <arbeers@wisc.edu>
2021-07-13 14:16:06 -0500

    Added get requests for dynamic html

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, session
-from flask import render_template
-import random
-import string
+from flask import Flask, session, render_template, jsonify, request
+from src.local import time, temp_conditions
+from src.configurator import configurator
+import os
 
+#Preload the user config and homescreen weather
+config = configurator()
+temp = temp_conditions(config.city)[0]
+
+#Start application
 app = Flask(__name__)
-letters = string.ascii_lowercase
-app.secret_key = ''.join(random.choice(letters) for i in range(15))
- 
+app.secret_key = os.urandom(16)
+
 @app.route("/")
 def home():
     if(session.get("vis", None) == None):
@@ -15,7 +19,7 @@ def home():
     else:
         val = session.get("vis", None)
         val2 = "none"
-    return render_template('index.html', vis = val, pointer = val2)
+    return render_template('index.html', vis = val, pointer = val2, time=time(), weather=str(temp) + "°")
 
 @app.route("/lights")
 def lights():
@@ -26,3 +30,21 @@ def lights():
 def weather():
     session["vis"] = 1
     return render_template('weather.html')
+
+@app.route("/email")
+def email():
+    session["vis"] = 1
+    return render_template('email.html')
+
+@app.route("/spotify")
+def spotify():
+    session["vis"] = 1
+    return render_template('spotify.html')
+
+@app.route('/update_home', methods = ['GET'])
+def update():
+    if(request.args.get('weather', 0, type=bool)):
+        temp = temp_conditions(config.city)[0]
+        return jsonify(time=time(), weather=temp)
+    else:
+        return jsonify(time=time(), weather=None)
