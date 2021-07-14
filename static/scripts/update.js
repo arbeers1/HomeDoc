2210d8665b159761db6abe394017380775f61899
Alex Beers <arbeers@wisc.edu>
2021-07-14 10:52:50 -0500

    Time update is now client sided

--- static/scripts/update.js
+++ static/scripts/update.js
@@ -1,26 +1,19 @@
+const date = new Date();
 /**
  * Updates elements on the home page.
  */
 function update_home(){
     interval = 0;
-    get_weather = false;
     setInterval(function(){ 
+
+        //Get temperature every 60 seconds
         if(interval == 60000){
-            get_weather = true;
             interval = 0;
-        }else{
-            get_weather = false;
+            $.getJSON($SCRIPT_ROOT + '/update_home', {
+            }, function(data) {
+                $("#temp").text(data.weather +"°");
+            }); 
         }
         interval++;
-
-        //Get request for time/weather. Weather only updates once every 5 mins.
-        $.getJSON($SCRIPT_ROOT + '/update_home', {
-            weather: get_weather
-        }, function(data) {
-          $("#time").text(data.time);
-          if(get_weather){
-              $("#temp").text(data.weather +"°");
-          }
-        });    
     }, 1000); 
 }
