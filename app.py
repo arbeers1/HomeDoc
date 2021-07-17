f9cff405c05ce568f1ecf607d4fd90514243a3a3
Alex Beers <arbeers@wisc.edu>
2021-07-17 17:40:54 -0500

    Set up light requests

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from threading import Condition
 from flask import Flask, session, render_template, jsonify, request
+from requests.api import get
 from src.local import *
+from src.controller import get_light_data, set_light_state, set_light_color, set_light_bri
 from src.configurator import configurator
 import os
 
@@ -21,6 +22,10 @@ def home():
         val = session.get("vis", None)
         val2 = "none"
     return render_template('index.html', vis = val, pointer = val2, time=time(), weather=str(temp) + "°")
+
+@app.route("/invis")
+def home_invis():
+    return render_template('index.html', vis = 0, pointer = "auto", time=time(), weather=str(temp) + "°")
 
 @app.route("/lights")
 def lights():
@@ -62,3 +67,16 @@ def spotify():
 def update():
     temp = temp_conditions(config.city)[0]
     return jsonify(weather=temp)
+
+@app.route('/update_lights', methods = ['GET', 'POST'])
+def update_lights():
+    if(request.method == 'GET'):
+        return jsonify(main=get_light_data())
+    if(request.method == 'POST'):
+        if(request.form.get('state') == "true"):
+            set_light_state(request.form.get('id'), request.form.get('state_val'))
+        elif(request.form.get('color') == "true"):
+            set_light_color(request.form.get('id'), request.form.get('color_val'))
+        elif(request.form.get('bri') == "true"):
+            set_light_bri(request.form.get('id'), request.form.get('bri_val'))
+        return jsonify(result="None")
